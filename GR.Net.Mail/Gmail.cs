acdf5ca42f0d1657432a1583468a3cdfed5b2a38
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2011-08-03 21:50:42 +0300

    GR.Net.Mail added

---
+++ GR.Net.Mail/Gmail.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace GR.Net.Mail
+{
+    /// <summary>
+    /// Simple Gmail client to send emails.
+    /// </summary>
+    public class Gmail
+    {
+        public static void SendMail(string userName, string password, string toAddress, string subject, string messageBody)
+        {
+            if (userName.IndexOf('@') == -1)
+                userName += "@gmail.com";
+
+            MailMessage mail = new MailMessage(userName, toAddress, subject, messageBody);
+
+            NetworkCredential networkCredential = new NetworkCredential(userName, password);
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = networkCredential;
+
+            smtpClient.Send(mail);
+        }
+
+        public static void SendMail(string userName, string password, string toAddress, string subject, string messageBody, string[] attachment_filenames)
+        {
+            if (userName.IndexOf('@') == -1)
+                userName += "@gmail.com";
+
+			using (MailMessage mail = new MailMessage(userName, toAddress, subject, messageBody))
+			{
+
+				foreach (string attachment_filename in attachment_filenames)
+				{
+					/*string mime_type = "application/unknown";
+                
+					string ext = System.IO.Path.GetExtension(attachment_filename).ToLower();
+                
+					Microsoft.Win32.RegistryKey reg_key = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+                
+					if (reg_key != null && reg_key.GetValue("Content Type") != null)
+						mime_type = reg_key.GetValue("Content Type").ToString();*/
+
+					mail.Attachments.Add(new Attachment(attachment_filename));
+				}
+
+				NetworkCredential networkCredential = new NetworkCredential(userName, password);
+				mail.IsBodyHtml = true;
+
+				SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+				smtpClient.UseDefaultCredentials = false;
+				smtpClient.EnableSsl = true;
+				smtpClient.Credentials = networkCredential;
+
+				smtpClient.Send(mail);
+			}
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ GR Botting Library
 ==================
 
 This projects contains parts of an automated gameplay (ie. botting) framework. It has been used for various problems and games, such as Poker, Backgammon and Blackjack. Parts of the code has been used by the author to create bots for fun for Bejeweled and Tetris Battle as well.
+
+Large part of the code has been in use last time in 2010 and hasn't been maintained or tested after that.
+
+GR.Net.Mail
+-----------
+
+Contains a simple GMail client for sending emails.
